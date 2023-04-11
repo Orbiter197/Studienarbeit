0a233d9e02b238c20c02346adb39f0acda98c131
Sviatoslav Shmarin <seshmarin@edu.hse.ru>
2023-04-11 15:33:36 +0300

    feat: interfaces for movement and visibility

---
+++ Project/Skizziere/Skizziere/Model/Notes/IRectangle.cs
@@ -0,0 +1,13 @@
+﻿namespace Skizziere.Model.Notes
+{
+    public interface IRectangle
+    {
+        public void Move(int dx, int dy);
+
+        public bool CheckIntersection(int x, int y);
+
+        public bool CheckIntersection(IRectangle rectangle);
+
+        public void Resize(int dLeft, int dTop, int dRight, int dBottom);
+    }
+}
