ef283ede7d82d75dead42bc220223223dff47f74
Sviatoslav Shmarin <seshmarin@edu.hse.ru>
2023-04-11 13:49:56 +0300

    feat: new root class for notes

---
+++ Project/Skizziere/Skizziere/Model/Shapes/BasicNote.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skizziere.Model.Shapes
+{
+    public abstract class BasicShape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public VisibilityState Visibility { get; set; }
+        public string VisibilityString
+        {
+            get
+            {
+                return VisibilityConverter.VisibilityToSting(Visibility);
+            }
+        }
+
+        public BasicShape() { 
+            X = 0;
+            Y = 0;
+            Width = 0; 
+            Height = 0;
+            Visibility = VisibilityState.Collapsed;
+        }
+
+        public BasicShape(int x, int y, int width, int height, VisibilityState visibilityState)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Visibility = visibilityState;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+        public void Resize(int dLeft, int dTop, int dRight, int dBottom)
+        {
+            X -= dLeft;
+            Y -= dTop;
+            Width += dRight;
+            Height += dBottom;
+        }
+    }
+}
